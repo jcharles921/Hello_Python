da7ce1660b7b231ee2dca119d79b1c72a24fb0b1
Charles <babouwitonze921@gmail.com>
2023-05-28 15:59:42 +0200

    feature UI template

--- UiForExam/urls.py
+++ UiForExam/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
+    path('clean/', views.clean, name='clean'),
+    path('accuracy/', views.accuracy, name='accuracy'),
+    path('createJoblib/', views.createJoblib, name='createJoblib'),
+    path('result/', views.result, name='result'),
 ]
